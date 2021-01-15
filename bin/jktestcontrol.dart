e55b007b49ca6759275094fa423e4a388485f302
Ruedy <>
2021-01-15 17:30:20 +0800

    userinfo接口

--- bin/jktestcontrol.dart
+++ bin/jktestcontrol.dart
@@ -9,7 +9,23 @@ class TestJkControl {
 
     print("---request--${request.url}");
 
+    if ('${request.url}' == 'userInfo') {
+      return json.encode(userInfo).toString();
+    }
+
 //    return "${request.url}";
     return json.encode(nihao).toString();
   }
+
+  static const Map userInfo = {
+    "code": 0,
+    "message": "success",
+    "data": {
+      "token": "54323rtnbgfvnbfvdc",
+      "name": "小明",
+      "age": 42,
+      "address": "中国上海",
+      "level": "13"
+    }
+  };
 }
