2036d41211932634469ed7b63854e8343b424fd9
Ruedy <ruedy.ding@wehotelglobal.com>
2021-01-24 16:51:07 +0800

    患者管理

--- bin/employments.dart
+++ bin/employments.dart
@@ -1,112 +1,118 @@
- const  Map
-employee={
-"code": 0,
-"message": "success",
-"data": {
-"employements": [
-{
-"yiyauan": "湖吉林医院大药房内科琦",
-"keshi": "综合内科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},
-{
-"yiyauan": "湖吉林医院大药房内科琦",
-"keshi": "综合内科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},
-{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-},{
-"yiyauan": "湖吉林医院大药2房内科琦2",
-"keshi": "综合内3科",
-"doctor":"王卓颖",
-"time":"2021-02-16 上午 8:00 - 08:15",
-"huanzhe":"chenayng",
-"address": "中国上海",
-"bgColor": "FFFEF3E1",
-"gender": "男",
-"healthUrl": "https://www.baidu.com"
-}
-]
-}
+const Map employee = {
+  "code": 0,
+  "message": "success",
+  "data": {
+    "employements": [
+      {
+        "yiyauan": "湖吉林医院大药房内科琦",
+        "keshi": "综合内科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "张三",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药房内科琦",
+        "keshi": "综合内科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "王六",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "韩城",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "刘磊",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "漆膜",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "胡诌",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "洛神",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "曹操",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "刘备",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      },
+      {
+        "yiyauan": "湖吉林医院大药2房内科琦2",
+        "keshi": "综合内3科",
+        "doctor": "王卓颖",
+        "time": "2021-02-16 上午 8:00 - 08:15",
+        "huanzhe": "貂蝉",
+        "address": "中国上海",
+        "bgColor": "FFFEF3E1",
+        "gender": "男",
+        "healthUrl": "https://www.baidu.com"
+      }
+    ]
+  }
 };
